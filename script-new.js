c336d80f17eb6639570dedceb852066ab9503f03
John Kiern <jkiernan12@yahoo.com>
2021-02-14 16:50:17 -0500

    Added click function to disable/enable 'painting' and improved appearence

--- script-new.js
+++ script-new.js
@@ -1,49 +1,29 @@
 var currentMode = "black"; //default coloring mode
+//global variables for easy experimenting
 let normalColor = "white"; //default grid color on reset
-let containerWidth = 400; //size of grid in px
-let container = document.getElementById("sketch-container");
+let containerWidth = 50; //size of grid; has to match stylesheet
+let containerUnits = "vw" //measurement unit for container
+
 //create grid of n x n blocks
 function createBlock(quantity) {
   let amount = quantity * quantity;
   let allBlocks = document.querySelectorAll(".block");
   allBlocks.forEach((block) => block.remove());
 
-  for (let i = 0; i < amount; i++) {
+  for (let i = 0; i < amount; i++) { //iterate through each block
     let block = document.createElement("div");
-
+    let container = document.getElementById("sketch-container");
     block.classList.add("block");
     container.appendChild(block);
-    blockSize = "" + containerWidth / quantity + "px";
+    blockSize = "" + containerWidth / quantity + containerUnits;
     block.style.width = blockSize;
     block.style.height = blockSize;
     block.addEventListener("mouseenter", colorBlock, false)//work
   }
 }
 
-container.addEventListener("click", clickTrue);
-function clickTrue() {
-  if (colorOn == true) {
-      container.classList.remove("paintbrush");
-    container.classList.add("select-cursor");
-colorOn = false;
-} else if (colorOn == false) {
-  container.classList.remove("select-cursor");
-  container.classList.add("paintbrush");
-colorOn = true;
-}
-}
-
-  var colorOn = false;
-function colorBlock() {
-  if (colorOn == true) {//determines each blocks color on hover
-  if (currentMode == "black"){
-  event.target.style.backgroundColor = "black";
-} else if (currentMode == "rainbow") {
-  event.target.style.backgroundColor = magicRainbow();
-} else if (currentMode == "custom") {
-  event.target.style.backgroundColor = colorChooser();
-}
-}
+function colorBlock() { //determines each blocks color on hover
+  event.target.style.backgroundColor = currentMode;
 }
 
 createBlock(16); //page loads with 16x16 grid
@@ -68,41 +48,29 @@ function gridSizer() {
   createBlock(gridInput);
 }
 
-// rainbow button and listener
-
-
-
-function magicRainbow(){
-  let rValue = Math.floor(Math.random() * 255);
-  let gValue = Math.floor(Math.random() * 255);
-  let bValue = Math.floor(Math.random() * 255);
-  let rgbValue = "rgb(" + rValue + ", " + gValue + ", " + bValue + ")";
-  return rgbValue;
-}
-//mode management
-//rainbow
+//rainbow button, listener and function
 let rainbowButton = document.querySelector("#rainbow");
 rainbowButton.addEventListener("click", rainbowTrue);
 
 function rainbowTrue() {
-  return currentMode = "rainbow";
+  let rValue = Math.floor(Math.random() * 255);
+  let gValue = Math.floor(Math.random() * 255);
+  let bValue = Math.floor(Math.random() * 255);
+  let rgbValue = "rgb(" + rValue + ", " + gValue + ", " + bValue + ")";
+  return currentMode = rgbValue;
 }
 
-//black
+//black button, listener and function
 let blackButton = document.querySelector("#black-btn");
 blackButton.addEventListener("click", blackTrue);
-
 function blackTrue() {
   return currentMode = "black";
 }
-//selected
+//selected button, listener and function
 let customColor = document.querySelector("#custom-color");
 customColor.addEventListener("input", customTrue);
 
 function customTrue() {
-  return currentMode = "custom";
-}
-function colorChooser (){
   colorChoice = "" + customColor.value + "";
-  return colorChoice;
+  return currentMode = colorChoice;
 }
